2905d11a1e7a10436b8dd681798c289d111316cc
joshua <joshua@tier27.com>
2015-04-13 18:18:11 -0400

    Filled out general page structure, tapped into some of the Angular functionality (not very much at present!), added images, etc.

--- js/main.js
+++ js/main.js
@@ -14,8 +14,9 @@ app.config(['$routeProvider', function ($routeProvider) {
 
     // Pages
     .when("/work", { templateUrl: "partials/work.html", controller: "PageCtrl" })
+    .when("/dog", { templateUrl: "partials/dog.html", controller: "PageCtrl" })
 
-    .when("/experiences", { templateUrl: "partials/experiences.html", controller: "PageCtrl" })
+    .when("/abilities", { templateUrl: "partials/abilities.html", controller: "PageCtrl" })
     .when("/blog", { templateUrl: "partials/blog.html", controller: "PageCtrl" })
     .when("/contact", { templateUrl: "partials/contact.html", controller: "PageCtrl" })
 
@@ -27,20 +28,49 @@ app.config(['$routeProvider', function ($routeProvider) {
 app.controller('PageCtrl', function ( $scope/*, $location, $http */) {
     console.log("Page Controller reporting for duty.");
     $scope.pageClass = 'page-effect';
+    $scope.title = 'Joshua Kornreich';
+    $scope.personal = {
+        name: 'Joshua Kornreich',
+        email: 'joshua@tier27.com',
+        address: '1021 PA Ave E, Warren, PA',
+        phone: '(954) 882-3115',
+    }
+
+    $scope.languages = [
+        { name: 'Python', level: 81, },
+        { name: 'MySQL', level: 79, },
+        { name: 'Javascript', level: 73, },
+        { name: 'PHP', level: 72, },
+        { name: 'Perl', level: 57, },
+        { name: 'Ruby', level: 49, },
+    ]
+
+    $scope.frameworks = [
+        { name: 'Django', level: 86, },
+        { name: 'jQuery', level: 85, },
+        { name: 'AngularJS', level: 78, },
+        { name: 'Laravel', level: 77, },
+        { name: 'Rails', level: 63, },
+        { name: 'Node.js', level: 53, },
+    ]
 
 });
 
 app.controller("dataImagesWork", function ($scope) {
     $scope.images_work = [
-          { num: 1, category: 'marketing', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 2, category: 'branding', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 3, category: 'design', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 4, category: 'photo', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 5, category: 'marketing', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 6, category: 'design', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 7, category: 'photo', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 8, category: 'marketing', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
-          { num: 9, category: 'design', src: "1100x1057", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html"}];
+          { num: 2, category: 'branding', src: "lms.png", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 1, category: 'marketing', src: "youstartme.png", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 3, category: 'design', src: "skava2.png", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          ];
+    $scope.images_dog = [
+          { num: 1, category: 'branding', src: "daisy5.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 2, category: 'design', src: "daisy7.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 3, category: 'photo', src: "daisy3.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 4, category: 'marketing', src: "daisy6.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 5, category: 'design', src: "daisy2.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 6, category: 'photo', src: "daisy9.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 7, category: 'marketing', src: "daisy8.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html" },
+          { num: 8, category: 'design', src: "daisy10.jpg", description: 'Oscar is a decent man. He used to clean porches with pleasure. ', url_details: "details.html"}];
 
 });
 
